651fdd090a3bde9cf62c1bb9375041b919ab3b22
Suminda Niroshan <sumindaniro@gmail.com>
2019-10-12 13:54:42 +0530

    Added package.json | Implemented time to live for Websocket connection table records for 10mins

--- connect_handler.py
+++ connect_handler.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+import time
 
 dynamodb = boto3.client('dynamodb')
 
@@ -8,6 +9,6 @@ def handle(event, context):
     connectionId = event['requestContext']['connectionId']
 
     # Insert the connectionId of the connected device to the database
-    dynamodb.put_item(TableName=os.environ['SOCKET_CONNECTIONS_TABLE_NAME'], Item={'connectionId': {'S': connectionId}})
+    dynamodb.put_item(TableName=os.environ['SOCKET_CONNECTIONS_TABLE_NAME'], Item={'connectionId': {'S': connectionId}, 'ttl': {'N': str(int(time.time() + 600))}})
 
     return {}
